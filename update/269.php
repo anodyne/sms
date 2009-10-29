6e4a0ea879803cde405dfc4fb1a51d07cfb2045e
David VanScott <davidv@anodyne-productions.com>
2009-10-28 22:34:22 -0400

    updated the changed files doc and 269 update page

--- update/269.php
+++ update/269.php
@@ -3,7 +3,7 @@
 Author: David VanScott [ davidv@anodyne-productions.com ]
 File: update/269.php
 Purpose: Update to 2.6.10
-Last Modified: 2009-09-16 1938 EST
+Last Modified: 2009-10-28 2215 EST
 **/
 
 /*
@@ -28,6 +28,6 @@ mysql_query("ALTER TABLE `sms_posts` CHANGE `postAuthor` `postAuthor` text NOT N
 | for this release.
 |
 */
-mysql_query("INSERT INTO sms_system_versions ( `version`, `versionDate`, `versionShortDesc`, `versionDesc` ) VALUES ('2.6.10', '', 'This release fixes bugs with ...', 'Fixed bug on news page where selecting a category would narrow down news but the category listed next to each news item wouldn't be accurate;Fixed bug in menu class where setting a general menu item to require login wouldn't allow anyone to see the link, logged in or not;Fixed bug where mission posts wouldn't be deleted when the delete action was triggered from the manage posts page;Fixed bug where post tags couldn't be updated from the Edit Mission Post page)");
+mysql_query("INSERT INTO sms_system_versions ( `version`, `versionDate`, `versionShortDesc`, `versionDesc` ) VALUES ('2.6.10', '', 'This release fixes bugs with ...', 'Fixed bug on news page where selecting a category would narrow down news but the category listed next to each news item wouldn't be accurate;Fixed bug in menu class where setting a general menu item to require login wouldn't allow anyone to see the link, logged in or not;Fixed bug where mission posts wouldn't be deleted when the delete action was triggered from the manage posts page;Fixed bug where post tags couldn't be updated from the Edit Mission Post page;Updated the database to set postAuthors as a text field;Added ability to change the number of authors on a post;Added loading graphic to the inbox to avoid blank pages while loading large inboxes;Changed install process so that the variables file doesn't have a closing PHP tag to prevent the output already sent errors)");
 
 ?>
