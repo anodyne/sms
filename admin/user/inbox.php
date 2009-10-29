97367094083fdbf10fe19f11e295acc92ce823f2
David VanScott <davidv@anodyne-productions.com>
2009-10-28 22:33:12 -0400

    updated the inbox with a loading graphic to prevent a blank screen while large inboxes are loading

--- admin/user/inbox.php
+++ admin/user/inbox.php
@@ -9,8 +9,8 @@ Author: David VanScott [ davidv@anodyne-productions.com ]
 File: admin/user/inbox.php
 Purpose: Page that views your private message inbox
 
-System Version: 2.6.1
-Last Modified: 2008-08-16 1655 EST
+System Version: 2.6.10
+Last Modified: 2009-10-28 2213 EST
 **/
 
 /* access check */
@@ -160,6 +160,15 @@ This private message was sent from " . printCrewNameEmail( $sessionCrewid ) . ".
 
 ?>
 
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#container-1 > ul').tabs(<?php echo $tab; ?>);
+			
+			$('#loader').hide();
+			$('#inbox').show();
+		});
+	</script>
+
 	<div class="body">
 		
 		<?
@@ -174,272 +183,272 @@ This private message was sent from " . printCrewNameEmail( $sessionCrewid ) . ".
 		
 		?>
 		
-		<span class="fontTitle">Private Messages</span><br />
+		<span class="fontTitle">Private Messages</span><br /><br />
 		
-		<script type="text/javascript">
-			$(document).ready(function(){
-				$('#container-1 > ul').tabs(<?php echo $tab; ?>);
-			});
-		</script>
+		<div id="loader" style="text-align:center;">
+			<img src="<?=$webLocation;?>images/loader.gif" alt="Loading..." class="image" /><br />
+			<span class="fontMedium bold">Loading, Please Wait...</span>
+		</div>
 		
-		<div id="container-1">
-			<ul>
-				<li><a href="#one"><span>Inbox<?php if( $msgCount[0] > 0 ) { echo " (" . $msgCount[0] . ")"; } ?></span></a></li>
-				<li><a href="#two"><span>Sent Messages</span></a></li>
-				<li><a href="#three"><span>Compose New Message</span></a></li>
-			</ul>
+		<div id="inbox" style="display:none;">
+			<div id="container-1">
+				<ul>
+					<li><a href="#one"><span>Inbox<?php if( $msgCount[0] > 0 ) { echo " (" . $msgCount[0] . ")"; } ?></span></a></li>
+					<li><a href="#two"><span>Sent Messages</span></a></li>
+					<li><a href="#three"><span>Compose New Message</span></a></li>
+				</ul>
 			
-			<div id="one" class="ui-tabs-container ui-tabs-hide">
-				<a href="javascript:selectAll(document.received,1);" class="fontNormal"><b>Select/Deselect All</b></a>
-				<br /><br />
+				<div id="one" class="ui-tabs-container ui-tabs-hide">
+					<a href="javascript:selectAll(document.received,1);" class="fontNormal"><b>Select/Deselect All</b></a>
+					<br /><br />
 				
-				<form method="post" name="received" action="<?=$webLocation;?>admin.php?page=user&sub=inbox&tab=1">
-				<table cellspacing="0" cellpadding="3">
-					<tr class="fontMedium">
-						<td width="4%"></td>
-						<td width="27%"><b>From</b></td>
-						<td width="70%"><b>Subject</b></td>
-					</tr>
-					<tr height="5">
-						<td colspan="3"></td>
-					</tr>
-					<?
+					<form method="post" name="received" action="<?=$webLocation;?>admin.php?page=user&sub=inbox&tab=1">
+					<table cellspacing="0" cellpadding="3">
+						<tr class="fontMedium">
+							<td width="4%"></td>
+							<td width="27%"><b>From</b></td>
+							<td width="70%"><b>Subject</b></td>
+						</tr>
+						<tr height="5">
+							<td colspan="3"></td>
+						</tr>
+						<?
 					
-					$rowCount = 1;
-					$color1 = "rowColor1";
-					$color2 = "rowColor2";
+						$rowCount = 1;
+						$color1 = "rowColor1";
+						$color2 = "rowColor2";
 					
-					/* loop through the results and fill the form */
-					while( $msgFetch = mysql_fetch_assoc( $getMessagesResult ) ) {
-						extract( $msgFetch, EXTR_OVERWRITE );
+						/* loop through the results and fill the form */
+						while( $msgFetch = mysql_fetch_assoc( $getMessagesResult ) ) {
+							extract( $msgFetch, EXTR_OVERWRITE );
 						
-						$rowColor = ( $rowCount % 2 ) ? $color1 : $color2;
+							$rowColor = ( $rowCount % 2 ) ? $color1 : $color2;
 			
-					?>
-					<tr height="40" class="<?=$rowColor;?>" <? if( $pmStatus == "unread" ) { echo "style='font-weight:bold;'"; } ?>>
-						<td align="center" valign="middle"><input type="checkbox" name="inbox_<?=$pmid;?>" value="<?=$pmid;?>" /></td>
-						<td>
-							<? printCrewName( $pmAuthor, "noRank", "noLink" ); ?><br />
-							<span class="fontSmall"><?=dateFormat( "medium", $pmDate );?></span>
-						</td>
-						<td>
-							<a href="<?=$webLocation;?>admin.php?page=user&sub=message&id=<?=$pmid;?>">
-								<?
+						?>
+						<tr height="40" class="<?=$rowColor;?>" <? if( $pmStatus == "unread" ) { echo "style='font-weight:bold;'"; } ?>>
+							<td align="center" valign="middle"><input type="checkbox" name="inbox_<?=$pmid;?>" value="<?=$pmid;?>" /></td>
+							<td>
+								<? printCrewName( $pmAuthor, "noRank", "noLink" ); ?><br />
+								<span class="fontSmall"><?=dateFormat( "medium", $pmDate );?></span>
+							</td>
+							<td>
+								<a href="<?=$webLocation;?>admin.php?page=user&sub=message&id=<?=$pmid;?>">
+									<?
 								
-								if( $pmStatus == "unread" ) {
-									echo "<img src='" . $webLocation . "images/message-unread-icon.png' border='0' alt='*' />";
-									echo "&nbsp;&nbsp;";
-								}
+									if( $pmStatus == "unread" ) {
+										echo "<img src='" . $webLocation . "images/message-unread-icon.png' border='0' alt='*' />";
+										echo "&nbsp;&nbsp;";
+									}
 			
-								if( !empty( $pmSubject ) ) {
-									printText( $pmSubject );
-								} else {
-									echo "<i>[ No Subject ]</i>";
-								}
+									if( !empty( $pmSubject ) ) {
+										printText( $pmSubject );
+									} else {
+										echo "<i>[ No Subject ]</i>";
+									}
 			
-								?>
-							</a><br />
-							<span class="fontSmall">
-								<?
+									?>
+								</a><br />
+								<span class="fontSmall">
+									<?
 								
-								/* build the snippet */
-								$length = 15; /* The number of words you want */
-								$words = explode( ' ', $pmContent ); /* Creates an array of words */
-								$wordsNew = array_slice( $words, 0, $length ); /* Slices the array */
-								$pmSnippet = implode( ' ', $wordsNew ); /* Grabs only the specified number of words */
+									/* build the snippet */
+									$length = 15; /* The number of words you want */
+									$words = explode( ' ', $pmContent ); /* Creates an array of words */
+									$wordsNew = array_slice( $words, 0, $length ); /* Slices the array */
+									$pmSnippet = implode( ' ', $wordsNew ); /* Grabs only the specified number of words */
 								
-								/* print out the snippet */
-								printText( $pmSnippet );
+									/* print out the snippet */
+									printText( $pmSnippet );
 								
-								/* if the snippet is longer than 10 words, display ellipses at the end */
-								if( count( $words ) > $length ) {
-									echo " ...";
-								}
+									/* if the snippet is longer than 10 words, display ellipses at the end */
+									if( count( $words ) > $length ) {
+										echo " ...";
+									}
 								
-								?>
-							</span>
-						</td>
-					</tr>
-					<? $rowCount++; } ?>
-					<tr>
-						<td colspan="4" height="15"></td>
-					</tr>
-					<tr>
-						<td colspan="4">
-							<input type="hidden" name="box" value="inbox" />
-							<input type="image" src="<?=path_userskin;?>buttons/remove.png" name="action" value="Remove" class="button" />
-						</td>
-					</tr>
-				</table>
-				</form>
-			</div>
+									?>
+								</span>
+							</td>
+						</tr>
+						<? $rowCount++; } ?>
+						<tr>
+							<td colspan="4" height="15"></td>
+						</tr>
+						<tr>
+							<td colspan="4">
+								<input type="hidden" name="box" value="inbox" />
+								<input type="image" src="<?=path_userskin;?>buttons/remove.png" name="action" value="Remove" class="button" />
+							</td>
+						</tr>
+					</table>
+					</form>
+				</div>
 			
-			<div id="two" class="ui-tabs-container ui-tabs-hide">
-				<a href="javascript:selectAll(document.sent,1);" class="fontNormal"><b>Select/Deselect All</b></a>
-				<br /><br />
+				<div id="two" class="ui-tabs-container ui-tabs-hide">
+					<a href="javascript:selectAll(document.sent,1);" class="fontNormal"><b>Select/Deselect All</b></a>
+					<br /><br />
 				
-				<form method="post" name="sent" action="<?=$webLocation;?>admin.php?page=user&sub=inbox&tab=2">
-				<table cellspacing="0" cellpadding="5" border="0">
-					<tr class="fontMedium">
-						<td width="4%"></td>
-						<td width="27%"><b>To</b></td>
-						<td width="70%"><b>Subject</b></td>
-					</tr>
-					<tr height="5">
-						<td colspan="3"></td>
-					</tr>
-					<?
+					<form method="post" name="sent" action="<?=$webLocation;?>admin.php?page=user&sub=inbox&tab=2">
+					<table cellspacing="0" cellpadding="5" border="0">
+						<tr class="fontMedium">
+							<td width="4%"></td>
+							<td width="27%"><b>To</b></td>
+							<td width="70%"><b>Subject</b></td>
+						</tr>
+						<tr height="5">
+							<td colspan="3"></td>
+						</tr>
+						<?
 					
-					$rowCount = 1;
-					$color1 = "rowColor1";
-					$color2 = "rowColor2";
+						$rowCount = 1;
+						$color1 = "rowColor1";
+						$color2 = "rowColor2";
 					
-					/* loop through the results and fill the form */
-					while( $msgOutFetch = mysql_fetch_assoc( $msgOutResult ) ) {
-						extract( $msgOutFetch, EXTR_OVERWRITE );
+						/* loop through the results and fill the form */
+						while( $msgOutFetch = mysql_fetch_assoc( $msgOutResult ) ) {
+							extract( $msgOutFetch, EXTR_OVERWRITE );
 			
-						$rowColor = ($rowCount % 2) ? $color1 : $color2;
+							$rowColor = ($rowCount % 2) ? $color1 : $color2;
 			
-					?>
-					<tr height="40" class="<?=$rowColor;?>">
-						<td align="center" valign="middle"><input type="checkbox" name="outbox_<?=$pmid;?>" value="<?=$pmid;?>" /></td>
-						<td>
-							<? printCrewName( $pmRecipient, "noRank", "link" ); ?><br />
-							<span class="fontSmall"><?=dateFormat( "medium", $pmDate );?></span>
-						</td>
-						<td valign="middle">
-							<a href="<?=$webLocation;?>admin.php?page=user&sub=message&id=<?=$pmid;?>">
-								<?
+						?>
+						<tr height="40" class="<?=$rowColor;?>">
+							<td align="center" valign="middle"><input type="checkbox" name="outbox_<?=$pmid;?>" value="<?=$pmid;?>" /></td>
+							<td>
+								<? printCrewName( $pmRecipient, "noRank", "link" ); ?><br />
+								<span class="fontSmall"><?=dateFormat( "medium", $pmDate );?></span>
+							</td>
+							<td valign="middle">
+								<a href="<?=$webLocation;?>admin.php?page=user&sub=message&id=<?=$pmid;?>">
+									<?
 								
-								if( !empty( $pmSubject ) ) {
-									printText( $pmSubject );
+									if( !empty( $pmSubject ) ) {
+										printText( $pmSubject );
+									} else {
+										echo "<i>[ No Subject ]</i>";
+									}
+								
+									?>
+								</a><br />
+								<span class="fontSmall">
+									<?
+								
+									/* build the snippet */
+									$length = 15; /* The number of words you want */
+									$words = explode( ' ', $pmContent ); /* Creates an array of words */
+									$wordsNew = array_slice( $words, 0, $length ); /* Slices the array */
+									$pmSnippet = implode( ' ', $wordsNew ); /* Grabs only the specified number of words */
+								
+									/* print out the snippet */
+									printText( $pmSnippet );
+								
+									/* if the snippet is longer than 10 words, display ellipses at the end */
+									if( count( $words ) > $length ) {
+										echo " ...";
+									}
+								
+									?>
+								</span>
+							</td>
+						</tr>
+						<? $rowCount++; } ?>
+						<tr>
+							<td colspan="3" height="20"></td>
+						</tr>
+						<tr>
+							<td colspan="3">
+								<input type="hidden" name="box" value="outbox" />
+								<input type="image" src="<?=path_userskin;?>buttons/remove.png" name="action" value="Remove" class="button" />
+							</td>
+						</tr>
+					</table>
+					</form>
+				</div>
+			
+				<div id="three" class="ui-tabs-container ui-tabs-hide">
+					<br />
+					<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=inbox&tab=3">
+					<table>
+						<tr>
+							<td class="narrowLabel tableCellLabel">To</td>
+							<td>&nbsp;</td>
+							<td>
+								<?
+			
+								if( !isset( $id ) ) {
+									print_active_crew_select_menu( "pm", "", "", "", "" );
 								} else {
-									echo "<i>[ No Subject ]</i>";
+									printCrewName( $id, "rank", "noLink" );
+									echo "<input type='hidden' name='pmRecipient' value='$id' />";
 								}
 								
 								?>
-							</a><br />
-							<span class="fontSmall">
+							</td>
+						</tr>
+						<tr height="5">
+							<td colspan="3"></td>
+						</tr>
+						<tr>
+							<td class="narrowLabel tableCellLabel">Subject</td>
+							<td>&nbsp;</td>
+							<td>
 								<?
-								
-								/* build the snippet */
-								$length = 15; /* The number of words you want */
-								$words = explode( ' ', $pmContent ); /* Creates an array of words */
-								$wordsNew = array_slice( $words, 0, $length ); /* Slices the array */
-								$pmSnippet = implode( ' ', $wordsNew ); /* Grabs only the specified number of words */
-								
-								/* print out the snippet */
-								printText( $pmSnippet );
-								
-								/* if the snippet is longer than 10 words, display ellipses at the end */
-								if( count( $words ) > $length ) {
-									echo " ...";
-								}
-								
-								?>
-							</span>
-						</td>
-					</tr>
-					<? $rowCount++; } ?>
-					<tr>
-						<td colspan="3" height="20"></td>
-					</tr>
-					<tr>
-						<td colspan="3">
-							<input type="hidden" name="box" value="outbox" />
-							<input type="image" src="<?=path_userskin;?>buttons/remove.png" name="action" value="Remove" class="button" />
-						</td>
-					</tr>
-				</table>
-				</form>
-			</div>
-			
-			<div id="three" class="ui-tabs-container ui-tabs-hide">
-				<br />
-				<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=inbox&tab=3">
-				<table>
-					<tr>
-						<td class="narrowLabel tableCellLabel">To</td>
-						<td>&nbsp;</td>
-						<td>
-							<?
-			
-							if( !isset( $id ) ) {
-								print_active_crew_select_menu( "pm", "", "", "", "" );
-							} else {
-								printCrewName( $id, "rank", "noLink" );
-								echo "<input type='hidden' name='pmRecipient' value='$id' />";
-							}
-								
-							?>
-						</td>
-					</tr>
-					<tr height="5">
-						<td colspan="3"></td>
-					</tr>
-					<tr>
-						<td class="narrowLabel tableCellLabel">Subject</td>
-						<td>&nbsp;</td>
-						<td>
-							<?
 							
-							/* do some logic to figure out what should go in the value field */
-							if( isset( $_GET['reply'] ) ) {
-								/* if there's already a reply string at the beginning, don't repeat it */
-								if( substr( $replysubject, 0, 4 ) == "RE: " ) {
-									$fieldValue = $replysubject;
+								/* do some logic to figure out what should go in the value field */
+								if( isset( $_GET['reply'] ) ) {
+									/* if there's already a reply string at the beginning, don't repeat it */
+									if( substr( $replysubject, 0, 4 ) == "RE: " ) {
+										$fieldValue = $replysubject;
+									} else {
+										/* if there isn't a reply string at the beginning, put it in */
+										$fieldValue = "RE: " . $replysubject;
+									}
 								} else {
-									/* if there isn't a reply string at the beginning, put it in */
-									$fieldValue = "RE: " . $replysubject;
+									/* otherwise, don't put anything in the value field */
+									$fieldValue = "";
 								}
-							} else {
-								/* otherwise, don't put anything in the value field */
-								$fieldValue = "";
-							}
 							
-							?>
-							<input type="text" class="name" name="pmSubject" style="font-weight:bold;" length="100" value="<?=$fieldValue;?>" />
-						</td>
-					</tr>
-					<tr height="5">
-						<td colspan="3"></td>
-					</tr>
-					<tr>
-						<td class="narrowLabel tableCellLabel">Message</td>
-						<td>&nbsp;</td>
-						<td><textarea name="pmContent" class="desc" rows="15"></textarea></td>
-					</tr>
-					<tr>
-						<td colspan="3" height="20"></td>
-					</tr>
-					<tr>
-						<td colspan="2">&nbsp;</td>
-						<td>
-							<input type="image" src="<?=path_userskin;?>buttons/send.png" name="action_send" value="Send" class="button" />
-						</td>
-					</tr>
-				</table>
-				</form>
+								?>
+								<input type="text" class="name" name="pmSubject" style="font-weight:bold;" length="100" value="<?=$fieldValue;?>" />
+							</td>
+						</tr>
+						<tr height="5">
+							<td colspan="3"></td>
+						</tr>
+						<tr>
+							<td class="narrowLabel tableCellLabel">Message</td>
+							<td>&nbsp;</td>
+							<td><textarea name="pmContent" class="desc" rows="15"></textarea></td>
+						</tr>
+						<tr>
+							<td colspan="3" height="20"></td>
+						</tr>
+						<tr>
+							<td colspan="2">&nbsp;</td>
+							<td>
+								<input type="image" src="<?=path_userskin;?>buttons/send.png" name="action_send" value="Send" class="button" />
+							</td>
+						</tr>
+					</table>
+					</form>
 				
-				<?php
+					<?php
 				
-				/* display the previous message if you're replying */
-				if(isset($reply))
-				{
-					$get = "SELECT * FROM sms_privatemessages WHERE pmid = $reply LIMIT 1";
-					$getR = mysql_query($get);
-					$fetch = mysql_fetch_assoc($getR);
+					/* display the previous message if you're replying */
+					if(isset($reply))
+					{
+						$get = "SELECT * FROM sms_privatemessages WHERE pmid = $reply LIMIT 1";
+						$getR = mysql_query($get);
+						$fetch = mysql_fetch_assoc($getR);
 					
-					echo "<br />";
-					echo "<div class='update notify-normal'>";
-					echo "<strong class='blue'>On " . dateFormat('long', $fetch['pmDate']) . " " . printCrewNameEmail($id) . " wrote:</strong><br /><br />";
-					printText($fetch['pmContent']);
-					echo "</div>";
-				}
+						echo "<br />";
+						echo "<div class='update notify-normal'>";
+						echo "<strong class='blue'>On " . dateFormat('long', $fetch['pmDate']) . " " . printCrewNameEmail($id) . " wrote:</strong><br /><br />";
+						printText($fetch['pmContent']);
+						echo "</div>";
+					}
 				
-				?>
+					?>
+				</div>
 			</div>
-			
 		</div>
 	
 <? } else { errorMessage( "private message inbox" ); } ?>
