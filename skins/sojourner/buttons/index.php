4e5408ac8db1d456f84af540f4ecb66199c9a21e
David VanScott <davidv@anodyne-productions.com>
2009-11-03 08:08:56 -0500

    removed the sojourner skin from the repo

--- skins/sojourner/buttons/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/* tell them to go away! */
-echo "You do not have permission to view this directory listing!";
-
-?>
--- skins/sojourner/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/* don't let anyone see the directory listing */
-echo "You are not authorized to view this directory.";
-
-?>
