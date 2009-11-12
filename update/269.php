cd15ce4e31fcafe768066ca33dd1dda0c602e2fc
David VanScott <davidv@anodyne-productions.com>
2009-11-12 00:27:29 -0500

    updated the 269 update file and changed files doc

--- update/269.php
+++ update/269.php
@@ -3,7 +3,7 @@
 Author: David VanScott [ davidv@anodyne-productions.com ]
 File: update/269.php
 Purpose: Update to 2.6.10
-Last Modified: 2009-11-04 0816 EST
+Last Modified: 2009-11-12 0023 EST
 **/
 
 /*
@@ -29,6 +29,6 @@ mysql_query("ALTER TABLE `sms_posts` CHANGE `postAuthor` `postAuthor` text NOT N
 |
 */
 
-mysql_query("INSERT INTO sms_system_versions ( `version`, `versionDate`, `versionShortDesc`, `versionDesc` ) VALUES ('2.6.10', '', 'This release fixes bugs with ...', 'Fixed bug on news page where selecting a category would narrow down news but the category listed next to each news item wouldn\'t be accurate;Fixed bug in menu class where setting a general menu item to require login wouldn\'t allow anyone to see the link, logged in or not;Fixed bug where mission posts wouldn\'t be deleted when the delete action was triggered from the manage posts page;Fixed bug where post tags couldn\'t be updated from the Edit Mission Post page;Updated the database to set postAuthors as a text field;Added ability to change the number of authors on a post;Added loading graphic to the inbox to avoid blank pages while loading large inboxes;Changed install process so that the variables file doesn\'t have a closing PHP tag to prevent the output already sent errors')");
+mysql_query("INSERT INTO sms_system_versions ( `version`, `versionDate`, `versionShortDesc`, `versionDesc` ) VALUES ('2.6.10', '', 'This release fixes bugs with ...', 'Fixed bug on news page where selecting a category would narrow down news but the category listed next to each news item wouldn\'t be accurate;Fixed bug in menu class where setting a general menu item to require login wouldn\'t allow anyone to see the link, logged in or not;Fixed bug where mission posts wouldn\'t be deleted when the delete action was triggered from the manage posts page;Fixed bug where post tags couldn\'t be updated from the Edit Mission Post page;Updated the database to set postAuthors as a text field;Added ability to change the number of authors on a post;Added loading graphic to the inbox to avoid blank pages while loading large inboxes;Changed install process so that the variables file doesn\'t have a closing PHP tag to prevent the output already sent errors;Updated the post count page to combine user records together by email address for compiling total count report')");
 
 ?>
